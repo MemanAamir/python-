980a5f9b685d1ec4d959e38351c6f513a294a6c2
Aamir <119026753+MemanAamir@users.noreply.github.com>
2022-11-25 05:18:45 +0000

    Pending changes exported from your codespace

---
+++ clac.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+    
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:"))
+print("Addition:",add(n1,n2))
+print("Substraction:",sub(n1,n2))
+print("Multipication:",mul(n1,n2))
+print("Division:",div(n1,n2))
---
+++ demo.py
@@ -0,0 +1,4 @@
+import calendar
+import time
+print(calendar.month(2022,7))
+print(time.asctime())
